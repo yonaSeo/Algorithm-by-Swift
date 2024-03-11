fc13271eaf350f88cb40f45efc91fe9b8cd4ddec
요나(Yona) <116647009+yonaSeo@users.noreply.github.com>
2024-03-11 10:25:26 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 8.16 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ left:Int, _ right:Int) -> Int {
+    let numbers = Array<Int>(left...right)
+    let factorsCount = numbers.map { number in
+        return (1...number).filter { number % $0 == 0 }.count
+	}
+    return numbers.enumerated().reduce(0) {
+        return $0 + (factorsCount[$1.offset] % 2 == 0 ? $1.element : -$1.element)
+    }
+}
