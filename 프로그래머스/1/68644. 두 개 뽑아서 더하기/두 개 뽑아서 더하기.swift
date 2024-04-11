9d1ed74a812ed204e9b6b6dc4f4dd004b6851d86
요나(Yona) <116647009+yonaSeo@users.noreply.github.com>
2024-04-11 14:23:32 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.39 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    var ans = Set<Int>()
+    for i in 0..<numbers.count {
+        for j in (i+1)..<numbers.count {
+            ans.insert(numbers[i]+numbers[j])
+        }
+    }
+    return ans.sorted()
+}
