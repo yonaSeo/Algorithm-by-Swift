9a5313fd6abb73482f5ea72b0a6d3da2214c7c10
요나(Yona) <116647009+yonaSeo@users.noreply.github.com>
2024-05-02 16:12:48 +0900

    [level 1] Title: 시저 암호, Time: 2.08 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+func solution(_ s:String, _ n:Int) -> String {
+    let a = Int(Character("a").asciiValue!)
+    let z = Int(Character("z").asciiValue!)
+    let A = Int(Character("A").asciiValue!)
+    let Z = Int(Character("Z").asciiValue!)
+    
+    let charArray = s.map { (char: Character) -> Character in
+        var ascii = Int(char.asciiValue!)
+        switch ascii {
+            case a...z:
+                if ascii + n > z { ascii -= (z-a+1) }
+            case A...Z:
+                if ascii + n > Z { ascii -= (Z-A+1) }
+            default:
+                return char
+        }
+        return Character(UnicodeScalar(ascii + n)!)
+    }
+    
+    return String(charArray)
+}
